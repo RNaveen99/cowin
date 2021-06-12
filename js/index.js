1ba1eaf08f038a95d72e63392bbc783121b56092
Naveen <naveenrohilla99@gmail.com>
2021-06-12 16:32:50 +0530

    init

---
+++ js/index.js
@@ -0,0 +1,42 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('serviceWorker.js')
+      .then((res) => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err));
+  });
+}
+
+const sound = new Audio('sound/beepSuccess.mp3');
+function checkSlot() {
+  let d = new Date();
+  let cowinURL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=124507&date=${d.getDate()}-${
+    d.getMonth() + 1
+  }-2021`;
+  console.log(cowinURL);
+  let count = 0;
+  axios
+    .get(cowinURL)
+    .then((res) => {
+      let result = res.data.sessions;
+      console.log('ok');
+      result.forEach((center) => {
+        if (center.available_capacity_dose1 > 0) {
+          count++;
+        }
+      });
+      if (count > 0) {
+        document.querySelector('body').innerHTML = `<h3>Count : ${count} </h3>`;
+        sound.play();
+      } else {
+        document.querySelector(
+          'body'
+        ).innerHTML = `<h3>Last fetched at ${new Date()}</h3>`;
+        sound.play();
+      }
+    })
+    .catch((err) => {
+      //
+    });
+}
+setInterval(checkSlot, 20000);
---
+++ serviceWorker.js
@@ -0,0 +1,20 @@
+const CowinAssests = 'Cowin';
+// const assets = ['index.html', './js/index.js'];
+const assets = ['index.html'];
+
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(CowinAssests).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
